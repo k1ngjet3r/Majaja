6ee042e7263f7ca57d6cdc1e96b5f21537a95efc
Jeter@work <43425262+k1ngjet3r@users.noreply.github.com>
2021-03-08 11:19:52 +0800

    Fail update

--- adb_command.py
+++ adb_command.py
@@ -18,20 +18,30 @@ def offline():
 
 def sign_in():
     adb_root()
-    os.system('adb shell input tap 0 600')
-    os.system('adb shell input tap 1850 200')
-    os.system('adb shell input tap 700 500')
+    # tap Google Maps
+    os.system('adb shell input tap 65 420')
+    # tap account icon
+    os.system('adb shell input tap 1692 144')
+    # tap sign in to Google
+    os.system('adb shell input tap 920 360')
     time.sleep(6)
-    os.system('adb shell input tap 700 700')
+    # tap sign in on car
+    os.system('adb shell input tap 590 500')
     time.sleep(6)
-    os.system('adb shell input tap 700 550')
+    # tap username input
+    os.system('adb shell input tap 420 400')
     time.sleep(3)
+    # enter username
     os.system('adb shell input text "gm.testing.phone"')
-    os.system('adb shell input tap 1850 200')
+    # tap next
+    os.system('adb shell input tap 1640 130')
     time.sleep(6)
+    # enter password
     os.system('adb shell input text "2019Go1101"')
-    os.system('adb shell input tap 1850 200')
+    # tap next 
+    os.system('adb shell input tap 1640 130')
     time.sleep(6)
+    # rap done
     os.system('adb shell input tap 1850 200')
 
 
